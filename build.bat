c6e7b93623b8680b9d0b60dc46f5bc36840f0a8c
Simon Howard <fraggle@soulsphere.org>
2019-11-29 17:11:58 -0500

    Simplify build batch file.
    Split into separate compile and linker stages so that we avoid
    rebuilding the same source file multiple times.

--- build.bat
+++ build.bat
@@ -1,8 +1,21 @@
-tcc ctrl\replay.c ctrl\control.c lib\flag.c
+tcc -c -oobj\flag     lib\flag.c
+tcc -c -oobj\control  ctrl\control.c
+tcc -c -oobj\replay   ctrl\replay.c
+tcc -c -oobj\doomnet  net\doomnet.c
+tcc -c -oobj\ipxnet   net\ipxnet.c
+tcc -c -oobj\ipxsetup net\ipxsetup.c
+tcc -c -oobj\parport  net\parport.c
+tcc -c -oobj\parsetup net\parsetup.c
+tasm /ml net\plio.asm obj\plio
+tcc -c -oobj\serport  net\serport.c
+tcc -c -oobj\sersetup net\sersetup.c
+rem tcc -c -oobj\solo-net net\solo-net.c
+tcc -c -oobj\statdump stat\statdump.c
+tcc -c -oobj\statprnt stat\statprnt.c
 
-tcc net\ipxsetup.c net\ipxnet.c net\doomnet.c lib\flag.c
-tcc net\parsetup.c net\parport.c net\doomnet.c net\plio.asm lib\flag.c
-tcc net\sersetup.c net\serport.c net\doomnet.c lib\flag.c
-rem tcc net\solo-net.c
-tcc stat\statdump.c stat\statprnt.c ctrl\control.c lib\flag.c
+tcc -oobj\replay   obj\replay.obj obj\control.obj obj\flag.obj
+tcc -oobj\ipxsetup obj\ipxsetup.obj obj\doomnet.obj obj\flag.obj obj\ipxnet.obj
+tcc -oobj\sersetup obj\sersetup.obj obj\doomnet.obj obj\flag.obj obj\serport.obj
+tcc -oobj\parsetup obj\parsetup.obj obj\doomnet.obj obj\flag.obj obj\parport.obj obj\plio.obj
+tcc -oobj\statdump obj\statdump.obj obj\control.obj obj\statprnt.obj obj\flag.obj
 

fragglet/vanilla-utilities

commit fe77d36cef90eccb24eaade1c7aee86c4c8fe634

Get rid of boolean enum types.
These are duplicated and redefined for both sersetup and parsetup, and
only used in a single function anyway.

net/sersetup.c
@@ -61,7 +61,7 @@ static int packetlen;
 static int inescape;
 static int newpacket;
 
-boolean ReadPacket(void)
+int ReadPacket(void)
 {
     int c;
 
@@ -70,8 +70,8 @@ boolean ReadPacket(void)
     if (inque.head - inque.tail > QUESIZE - 4)  // check for buffer overflow
     {
         inque.tail = inque.head;
-        newpacket = true;
-        return false;
+        newpacket = 1;
+        return 0;
     }
 
     if (newpacket)
@@ -84,20 +84,20 @@ boolean ReadPacket(void)
     {
         c = ReadByte();
         if (c < 0)
-            return false;       // haven't read a complete packet
+            return 0;       // haven't read a complete packet
         //printf ("%c",c);
         if (inescape)
         {
-            inescape = false;
+            inescape = 0;
             if (c != FRAMECHAR)
             {
                 newpacket = 1;
-                return true;    // got a good packet
+                return 1;    // got a good packet
             }
         }
         else if (c == FRAMECHAR)
         {
-            inescape = true;
+            inescape = 0;
             continue;           // don't know yet if it is a terminator
         }                       // or a literal FRAMECHAR
 
@@ -106,7 +106,6 @@ boolean ReadPacket(void)
         packet[packetlen] = c;
         packetlen++;
     } while (1);
-
 }
 
 /*

net/parsetup.c
@@ -51,17 +51,15 @@ extern unsigned errcnt;
 
 #define MAXPACKET	512
 
-boolean ReadPacket(void)
+int ReadPacket(void)
 {
-
     if (newpkt)
     {
         newpkt = 0;
-        return true;            // true - got a good packet
+        return 1;            // true - got a good packet
     }
 
-    return (false);             // false - no packet available
-
+    return 0;                // false - no packet available
 }
 
 /*

net/parsetup.h
@@ -5,8 +5,6 @@
 //
 //
 
-typedef enum { false, true } boolean;
-
 void ParallelRegisterFlags(void);
 void InitPort(void);
 void ShutdownPort(void);

net/sersetup.h
@@ -1,8 +1,6 @@
 #define INPUT( port )        inp( port )
 #define OUTPUT( port, data ) (void) outp( port, data )
 
-typedef enum { false, true } boolean;
-
 #define TRANSMIT_HOLDING_REGISTER            0x00
 #define RECEIVE_BUFFER_REGISTER              0x00
 #define INTERRUPT_ENABLE_REGISTER            0x01

commit 5193fd6fc5488c64cc0b384363578f8c6e816f24

sersetup: Fix bug introduced in fe77d36cef90eccb2.

net/sersetup.c
@@ -97,7 +97,7 @@ int ReadPacket(void)
         }
         else if (c == FRAMECHAR)
         {
-            inescape = 0;
+            inescape = 1;
             continue;           // don't know yet if it is a terminator
         }                       // or a literal FRAMECHAR
 
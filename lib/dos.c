fragglet/vanilla-utilities

commit ca7f919e6d01e9f5ad4ee71b4e7fbe7dd74e1171

Use _bios_keybrd() and _bios_timeofday().
These are the Microsoft C names; all other DOS compilers support
these for compatibility.

lib/dos.c
@@ -54,7 +54,7 @@ int FindAndHookInterrupt(struct interrupt_hook *state,
 
 long GetEntropy(void)
 {
-    return biostime(0, 0);
+    return _bios_timeofday(_TIME_GETCLOCK, NULL);
 }
 
 void RestoreInterrupt(struct interrupt_hook *state)

lib/log.c
@@ -104,9 +104,9 @@ void ErrorPrintUsage(char *fmt, ...)
 
 void CheckAbort(char *operation)
 {
-    while (bioskey(1))
+    while (_bios_keybrd(_KEYBRD_READY))
     {
-        if ((bioskey(0) & 0xff) == 27)
+        if ((_bios_keybrd(_KEYBRD_READ) & 0xff) == 27)
         {
             Error("%s aborted.", operation);
         }

commit 2e1eb8d109630a417b1a435a52c425c01e5bf7d5

Fix GetEntropy() bug introduced in ca7f919e6d01e.
_bios_timeofday() does not work the same as biostime() since the
retrieved time is stored in a pointer address rather than being returned
directly from the function. This fixes "Null pointer assignment" errors
that were appearing on exit from metanet.

lib/dos.c
@@ -54,7 +54,9 @@ int FindAndHookInterrupt(struct interrupt_hook *state,
 
 long GetEntropy(void)
 {
-    return _bios_timeofday(_TIME_GETCLOCK, NULL);
+    long result;
+    _bios_timeofday(_TIME_GETCLOCK, &result);
+    return result;
 }
 
 void RestoreInterrupt(struct interrupt_hook *state)
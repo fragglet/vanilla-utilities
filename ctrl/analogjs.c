fragglet/vanilla-utilities

commit ef7568a9627db62ac25df330e67324d5d5e3a5db

net: Move NetISR() into doomnet.c and pass callback.
Users of the network API should be able to use the doomnet.c API
functions without implementing the NetISR() function, so instead
pass a callback function that callers will supply to LaunchDOOM.
This enforces a more clean API separation.

net/doomnet.c
@@ -10,6 +10,7 @@
 #include "net/doomnet.h"
 
 static struct interrupt_hook net_interrupt;
+static void (*isr_callback)(void);
 static int dup = 0, extratics = 0;
 
 void NetRegisterFlags(void)
@@ -26,6 +27,11 @@ void NetRegisterFlags(void)
 static void UnhookDoomVector(void)
 {
     RestoreInterrupt(&net_interrupt);
+}
+
+static void interrupt far NetISR(void)
+{
+    isr_callback();
 }
 
 /*
@@ -46,10 +52,13 @@ These fields in doomcom should be filled in before calling:
 =============
 */
 
-void LaunchDOOM(doomcom_t far *doomcom, char **args)
+void LaunchDOOM(doomcom_t far *doomcom, char **args,
+                void (*callback)(void))
 {
     char addrstring[10];
     long flatadr;
+
+    isr_callback = callback;
 
     if (dup != 0)
     {

net/doomnet.h
@@ -77,8 +77,8 @@ typedef struct
 } doompacket_t;
 
 void NetRegisterFlags(void);
-void LaunchDOOM(doomcom_t far *doomcom, char **args);
-void interrupt far NetISR(void);
+void LaunchDOOM(doomcom_t far *doomcom, char **args,
+                void (*callback)(void));
 doomcom_t far *NetGetHandle(long l);
 void NetSendPacket(doomcom_t far *doomcom);
 int NetGetPacket(doomcom_t far *doomcom);

net/ipxsetup.c
@@ -17,15 +17,7 @@ static int numnetnodes;
 
 static setupdata_t nodesetup[MAXNETNODES];
 
-/*
-=============
-=
-= NetISR
-=
-=============
-*/
-
-void interrupt far NetISR(void)
+static void NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {
@@ -225,6 +217,6 @@ void main(int argc, char *argv[])
     //
     // launch DOOM
     //
-    LaunchDOOM(&doomcom, args);
+    LaunchDOOM(&doomcom, args, NetCallback);
 }
 

net/metanet.c
@@ -792,7 +792,7 @@ static void PrintStats(void)
     }
 }
 
-void interrupt far NetISR(void)
+static void NetCallback(void)
 {
     switch (doomcom.command)
     {
@@ -895,7 +895,7 @@ int main(int argc, char *argv[])
     {
         LogMessage("Console is player %d of %d (%d nodes)",
                    doomcom.consoleplayer, doomcom.numplayers, num_nodes);
-        LaunchDOOM(&doomcom, args);
+        LaunchDOOM(&doomcom, args, NetCallback);
     }
 
     PrintStats();

net/parsetup.c
@@ -81,15 +81,7 @@ int WritePacket(uint8_t *data, unsigned len)
     return PLIOWritePacket();
 }
 
-/*
-=============
-=
-= NetISR
-=
-=============
-*/
-
-void interrupt far NetISR(void)
+static void NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {
@@ -229,6 +221,6 @@ void main(int argc, char *argv[])
     //
     // launch DOOM
     //
-    LaunchDOOM(&doomcom, args);
+    LaunchDOOM(&doomcom, args, NetCallback);
 }
 

net/passthru.c
@@ -14,7 +14,7 @@
 static doomcom_t doomcom;
 static doomcom_t far *inner_driver;
 
-void interrupt NetISR(void)
+static void NetCallback(void)
 {
     switch (doomcom.command)
     {
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
     doomcom.ticdup = inner_driver->ticdup;
     doomcom.extratics = inner_driver->extratics;
 
-    LaunchDOOM(&doomcom, args);
+    LaunchDOOM(&doomcom, args, NetCallback);
 
     return 0;
 }

net/sersetup.c
@@ -139,15 +139,7 @@ void WritePacket(char *buffer, int len)
     WriteBuffer(localbuffer, b);
 }
 
-/*
-=============
-=
-= NetISR
-=
-=============
-*/
-
-void interrupt far NetISR(void)
+static void NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {
@@ -457,5 +449,5 @@ void main(int argc, char *argv[])
     //
     // launch DOOM
     //
-    LaunchDOOM(&doomcom, args);
+    LaunchDOOM(&doomcom, args, NetCallback);
 }

net/solo-net.c
@@ -151,7 +151,7 @@ static void ReceivePacket(void)
 // to send or receive a packet.
 // If numnodes=1 then this can just be an empty function. We only bother
 // doing anything so that we can simulate extra dummy players.
-void interrupt far NetISR(void)
+static void NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {
@@ -203,7 +203,7 @@ int main(int argc, char *argv[])
     doomcom.numplayers = nodes;
     doomcom.numnodes = nodes;
 
-    LaunchDOOM(&doomcom, args);
+    LaunchDOOM(&doomcom, args, NetCallback);
 
     return 0;
 }

commit 41f591aeade5dde8d9a066d30960dad0bbb3ed83

Make callbacks invoked in ISRs far pointers.
We cannot rely on invoking near function pointers inside ISRs since the
segment pointers may be completely different. This has become apparent
as ef7568a9627db62a broke the network drivers, causing any invocations
of the network ISR to just hang. So fix this but also fix the control
API as well while we're at it since it uses the same mechanism.

ctrl/analogjs.c
@@ -190,7 +190,7 @@ static int16_t AdjustAxisValue(struct axis *a, int v, int speed)
     (b < 0 ? 0 : \
      b > 20 ? 1 : (buttons & (1 << b)) == 0)
 
-void ControlCallback(ticcmd_t *ticcmd)
+static void far ControlCallback(ticcmd_t *ticcmd)
 {
     int buttons;
     int run;

ctrl/control.h
@@ -68,7 +68,7 @@ typedef struct {
     int32_t inventoryitem;
 } ticcmd_t;
 
-typedef void (*control_callback_t)(ticcmd_t *ticcmd);
+typedef void (far *control_callback_t)(ticcmd_t *ticcmd);
 
 void ControlRegisterFlags(void);
 void ControlLaunchDoom(char **args, control_callback_t callback);

ctrl/replay.c
@@ -37,7 +37,7 @@ static uint8_t *demo_p;
 static char *demo_filename = NULL;
 static int is_strife = 0;
 
-static void ReplayCallback(ticcmd_t *ticcmd)
+static void far ReplayCallback(ticcmd_t *ticcmd)
 {
     int space_needed;
 

net/doomnet.c
@@ -10,7 +10,7 @@
 #include "net/doomnet.h"
 
 static struct interrupt_hook net_interrupt;
-static void (*isr_callback)(void);
+static void (far *isr_callback)(void);
 static int dup = 0, extratics = 0;
 
 void NetRegisterFlags(void)
@@ -53,7 +53,7 @@ These fields in doomcom should be filled in before calling:
 */
 
 void NetLaunchDoom(doomcom_t far *doomcom, char **args,
-                   void (*callback)(void))
+                   void (far *callback)(void))
 {
     char addrstring[10];
     long flatadr;

net/doomnet.h
@@ -78,7 +78,7 @@ typedef struct
 
 void NetRegisterFlags(void);
 void NetLaunchDoom(doomcom_t far *doomcom, char **args,
-                   void (*callback)(void));
+                   void (far *callback)(void));
 doomcom_t far *NetGetHandle(long l);
 void NetSendPacket(doomcom_t far *doomcom);
 int NetGetPacket(doomcom_t far *doomcom);

net/ipxsetup.c
@@ -17,7 +17,7 @@ static int numnetnodes;
 
 static setupdata_t nodesetup[MAXNETNODES];
 
-static void NetCallback(void)
+static void far NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {

net/metanet.c
@@ -792,7 +792,7 @@ static void PrintStats(void)
     }
 }
 
-static void NetCallback(void)
+static void far NetCallback(void)
 {
     switch (doomcom.command)
     {

net/parsetup.c
@@ -81,7 +81,7 @@ int WritePacket(uint8_t *data, unsigned len)
     return PLIOWritePacket();
 }
 
-static void NetCallback(void)
+static void far NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {

net/passthru.c
@@ -14,7 +14,7 @@
 static doomcom_t doomcom;
 static doomcom_t far *inner_driver;
 
-static void NetCallback(void)
+static void far NetCallback(void)
 {
     switch (doomcom.command)
     {

net/sersetup.c
@@ -139,7 +139,7 @@ void WritePacket(char *buffer, int len)
     WriteBuffer(localbuffer, b);
 }
 
-static void NetCallback(void)
+static void far NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {

net/solo-net.c
@@ -151,7 +151,7 @@ static void ReceivePacket(void)
 // to send or receive a packet.
 // If numnodes=1 then this can just be an empty function. We only bother
 // doing anything so that we can simulate extra dummy players.
-static void NetCallback(void)
+static void far NetCallback(void)
 {
     if (doomcom.command == CMD_SEND)
     {

stat/stats.c
@@ -21,7 +21,7 @@ static wbstartstruct_t stats_buffer;
 // buffer has been written to.  If it has, save the contents of the
 // stats buffer into the captured_stats array for later processing.
 ///
-static void ControlCallback(ticcmd_t *unused)
+static void far ControlCallback(ticcmd_t *unused)
 {
     unused = unused;
 
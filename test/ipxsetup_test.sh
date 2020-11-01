eb4d37482f85559fb5154d0668c7eda449a7766b
Simon Howard <fraggle@soulsphere.org>
2020-11-01 14:48:14 -0500

    Fix up tests for case sensitive filesystems.
    Programs in dosbox will write files with all-uppercase names, so if the
    test is being run on a system with a case sensitive filesystem the output
    files written by fakedoom.exe won't be found.

--- test/ipxsetup_test.sh
+++ test/ipxsetup_test.sh
@@ -13,7 +13,7 @@ ipx = true
 [autoexec]
 $AUTOEXEC_BOILERPLATE
 ipxnet startserver $TEST_PORT
-ipxsetup -nodes 8 fakedoom -out t:server.txt -secret 1000
+ipxsetup -nodes 8 fakedoom -out t:SERVER.TXT -secret 1000
 exit
 END
 
@@ -27,7 +27,7 @@ for i in $(seq 7); do
 	[autoexec]
 	$AUTOEXEC_BOILERPLATE
 	ipxnet connect 127.0.0.1 $TEST_PORT
-	ipxsetup -nodes 8 fakedoom -out t:client$i.txt -secret $((1000 + i))
+	ipxsetup -nodes 8 fakedoom -out t:CLIENT$I.TXT -secret $((1000 + i))
 	exit
 END
 done
@@ -37,11 +37,11 @@ wait_dosboxes
 # Check the log files look as expected.
 # All players generate an identical log file and all secrets are included.
 for i in $(seq 7); do
-	diff -u $TEST_DIR/server.txt $TEST_DIR/client$i.txt
+	diff -u $TEST_DIR/SERVER.TXT $TEST_DIR/CLIENT$I.TXT
 done
 
 for i in $(seq 0 7); do
-	grep -qw $((1000 + i)) $TEST_DIR/server.txt
+	grep -qw $((1000 + i)) $TEST_DIR/SERVER.TXT
 done
 
 
--- test/sersetup_test.sh
+++ test/sersetup_test.sh
@@ -12,7 +12,7 @@ serial1 = modem listenport:$TEST_PORT
 
 [autoexec]
 $AUTOEXEC_BOILERPLATE
-sersetup -answer fakedoom -out t:answer.txt -secret 1000
+sersetup -answer fakedoom -out t:ANSWER.TXT -secret 1000
 exit
 END
 
@@ -24,13 +24,13 @@ serial1 = modem
 
 [autoexec]
 $AUTOEXEC_BOILERPLATE
-sersetup -dial 127.0.0.1:$TEST_PORT fakedoom -out t:dial.txt -secret 2000
+sersetup -dial 127.0.0.1:$TEST_PORT fakedoom -out t:DIAL.TXT -secret 2000
 exit
 END
 
 wait_dosboxes
 
-diff -u $TEST_DIR/answer.txt $TEST_DIR/dial.txt
-grep -q 1000 $TEST_DIR/answer.txt
-grep -q 2000 $TEST_DIR/answer.txt
+diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+grep -q 1000 $TEST_DIR/ANSWER.TXT
+grep -q 2000 $TEST_DIR/ANSWER.TXT
 

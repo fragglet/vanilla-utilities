019cfcc4172d2c27a16582bd2fb8f26504588be6
Simon Howard <fraggle@soulsphere.org>
2020-11-08 22:39:25 -0500

    Shrink ISR stack space.
    I previously thought 2KiB of stack was necessary but I think I only
    had it set this large because of the Watcom stack overflow bug that
    was fixed in 3ca32735df0a3580f289b6fb1aa452cc15d2f1cc.

--- lib/ints.h
+++ lib/ints.h
@@ -1,7 +1,7 @@
 
 #include <dos.h>
 
-#define ISR_STACK_SIZE 2048
+#define ISR_STACK_SIZE 1024
 
 /* SwitchStack() should be called at the start of an ISR function to swap
  * out the stack pointer to point to the isr_stack_space memory block.

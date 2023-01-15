abacbadffe1c519bdb0071ee2040e67baef8892d
Simon Howard <fraggle@gmail.com>
2023-01-15 16:25:47 -0500

    Add more SERSETUP tests.

--- test/sersetup_test.sh
+++ test/sersetup_test.sh
@@ -14,19 +14,52 @@ set -eu
 
 . test/common.sh
 
-dosbox_with_conf "$SERVER_OPTIONS" <<END
-sersetup -answer fakedoom -out t:ANSWER.TXT -secret 1000
+start_answerer() {
+    dosbox_with_conf "$SERVER_OPTIONS" <<END
+      sersetup -answer $@ fakedoom -out t:ANSWER.TXT -secret 1000
 END
+    sleep 1
+}
 
-sleep 1
-
-dosbox_with_conf "$CLIENT_OPTIONS" <<END
-sersetup -dial 127.0.0.1:$TEST_PORT fakedoom -out t:DIAL.TXT -secret 2000
+start_dialer() {
+    dosbox_with_conf "$CLIENT_OPTIONS" <<END
+      sersetup $@ -dial localhost:$TEST_PORT fakedoom -out t:DIAL.TXT -secret 2000
 END
+}
 
+# Simple dial and answer.
+start_answerer
+start_dialer
 wait_dosboxes
 
 diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
-grep -q 1000 $TEST_DIR/ANSWER.TXT
-grep -q 2000 $TEST_DIR/ANSWER.TXT
+grep -q secret=1000 $TEST_DIR/ANSWER.TXT
+grep -q secret=2000 $TEST_DIR/ANSWER.TXT
+
+# Force player at answer.
+start_answerer -player1
+start_dialer
+wait_dosboxes
+
+diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+grep -q "Player 1: secret=1000" $TEST_DIR/ANSWER.TXT
+grep -q "Player 2: secret=2000" $TEST_DIR/ANSWER.TXT
+
+# Force player at dial.
+start_answerer
+start_dialer -player2
+wait_dosboxes
+
+diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+grep -q "Player 1: secret=1000" $TEST_DIR/ANSWER.TXT
+grep -q "Player 2: secret=2000" $TEST_DIR/ANSWER.TXT
+
+# Force player at both works as long as they're consistent.
+start_answerer -player2
+start_dialer -player1
+wait_dosboxes
+
+diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+grep -q "Player 1: secret=2000" $TEST_DIR/ANSWER.TXT
+grep -q "Player 2: secret=1000" $TEST_DIR/ANSWER.TXT
 

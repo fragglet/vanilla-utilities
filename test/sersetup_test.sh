78230918255f69f8665469e5b430a2937d2c6a83
Simon Howard <fraggle@gmail.com>
2023-01-19 13:55:25 -0500

    sersetup: Add testing for -bg and null modem connections.
    Fixes #14.

--- test/sersetup_test.sh
+++ test/sersetup_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-TEST_PORT=30328
+TEST_PORT1=30328
+TEST_PORT2=30329
 
 set -eu
 
@@ -8,8 +9,9 @@ set -eu
 
 start_answerer() {
     start_dosbox <<END
-      serial1 modem listenport:$TEST_PORT
-      sersetup -answer $@ fakedoom -out t:ANSWER.TXT -secret 1000
+      serial1 modem listenport:$TEST_PORT1
+      serial2 nullmodem port:$TEST_PORT2
+      sersetup $@ fakedoom -out t:ANSWER.TXT -secret 1000
 END
     sleep 1
 }
@@ -17,43 +19,52 @@ END
 start_dialer() {
     start_dosbox <<END
       serial1 modem
-      sersetup $@ -dial localhost:$TEST_PORT fakedoom -out t:DIAL.TXT -secret 2000
+      serial2 nullmodem server:localhost port:$TEST_PORT2
+      sersetup $@ fakedoom -out t:DIAL.TXT -secret 2000
 END
 }
 
-# Simple dial and answer.
-start_answerer
-start_dialer
-wait_dosboxes
+run_player_tests() {
+    # Simple dial and answer.
+    start_answerer $1
+    start_dialer $2
+    wait_dosboxes
 
-diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
-grep -q secret=1000 $TEST_DIR/ANSWER.TXT
-grep -q secret=2000 $TEST_DIR/ANSWER.TXT
+    diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+    grep -q secret=1000 $TEST_DIR/ANSWER.TXT
+    grep -q secret=2000 $TEST_DIR/ANSWER.TXT
 
-# Force player at answer.
-start_answerer -player1
-start_dialer
-wait_dosboxes
+    # Force player at answer.
+    start_answerer $1 -player1
+    start_dialer $2
+    wait_dosboxes
 
-diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
-grep -q "Player 1: secret=1000" $TEST_DIR/ANSWER.TXT
-grep -q "Player 2: secret=2000" $TEST_DIR/ANSWER.TXT
+    diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+    grep -q "Player 1: secret=1000" $TEST_DIR/ANSWER.TXT
+    grep -q "Player 2: secret=2000" $TEST_DIR/ANSWER.TXT
 
-# Force player at dial.
-start_answerer
-start_dialer -player2
-wait_dosboxes
+    # Force player at dial.
+    start_answerer $1
+    start_dialer $2 -player2
+    wait_dosboxes
 
-diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
-grep -q "Player 1: secret=1000" $TEST_DIR/ANSWER.TXT
-grep -q "Player 2: secret=2000" $TEST_DIR/ANSWER.TXT
+    diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+    grep -q "Player 1: secret=1000" $TEST_DIR/ANSWER.TXT
+    grep -q "Player 2: secret=2000" $TEST_DIR/ANSWER.TXT
 
-# Force player at both works as long as they're consistent.
-start_answerer -player2
-start_dialer -player1
-wait_dosboxes
+    # Force player at both works as long as they're consistent.
+    start_answerer $1 -player2
+    start_dialer $2 -player1
+    wait_dosboxes
 
-diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
-grep -q "Player 1: secret=2000" $TEST_DIR/ANSWER.TXT
-grep -q "Player 2: secret=1000" $TEST_DIR/ANSWER.TXT
+    diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT
+    grep -q "Player 1: secret=2000" $TEST_DIR/ANSWER.TXT
+    grep -q "Player 2: secret=1000" $TEST_DIR/ANSWER.TXT
+}
+
+run_player_tests "-answer" "-dial localhost:$TEST_PORT1"
+run_player_tests "-bg -answer" "-bg -dial localhost:$TEST_PORT1"
+
+# For testing null modem connections we just use COM2.
+run_player_tests "-com2" "-com2"
 

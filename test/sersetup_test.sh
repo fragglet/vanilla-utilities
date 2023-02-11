19847cef5970a52af9770a7d5076480b915f4c03
Simon Howard <fraggle@gmail.com>
2023-02-11 15:03:17 -0500

    Rename "answerer" to "node1", "dialer" to "node2".
    The nullmodem tests do not use -answer/-dial so these names were misleading.

--- test/sersetup_test.sh
+++ test/sersetup_test.sh
@@ -7,75 +7,75 @@ set -eu
 
 . test/common.sh
 
-start_answerer() {
+start_node1() {
     start_dosbox <<END
       config -set serial serial1 modem listenport:$TEST_PORT1
       config -set serial serial2 nullmodem port:$TEST_PORT2
-      sersetup $@ fakedoom -out t:ANSWER.TXT -secret 1000
+      sersetup $@ fakedoom -out t:NODE1.TXT -secret 1000
 END
     sleep 1
 }
 
-start_dialer() {
+start_node2() {
     start_dosbox <<END
       config -set serial serial1 modem
       config -set serial serial2 nullmodem server:localhost port:$TEST_PORT2
-      sersetup $@ fakedoom -out t:DIAL.TXT -secret 2000
+      sersetup $@ fakedoom -out t:NODE2.TXT -secret 2000
 END
 }
 
 run_player_tests() {
     # Simple dial and answer.
-    start_answerer $1
-    start_dialer $2
+    start_node1 $1
+    start_node2 $2
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT ||
-       ! grep -q secret=1000 $TEST_DIR/ANSWER.TXT ||
-       ! grep -q secret=2000 $TEST_DIR/ANSWER.TXT; then
+    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
+       ! grep -q secret=1000 $TEST_DIR/NODE1.TXT ||
+       ! grep -q secret=2000 $TEST_DIR/NODE1.TXT; then
         echo "Wrong or missing secrets for '$1' '$2' #1: "
-        diff -u /dev/null $TEST_DIR/ANSWER.TXT
+        diff -u /dev/null $TEST_DIR/NODE1.TXT
         exit 1
     fi
 
     # Force player at answer.
-    start_answerer $1 -player2
-    start_dialer $2
+    start_node1 $1 -player2
+    start_node2 $2
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT ||
-       ! grep -q "Player 1: secret=2000" $TEST_DIR/ANSWER.TXT ||
-       ! grep -q "Player 2: secret=1000" $TEST_DIR/ANSWER.TXT; then
+    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
+       ! grep -q "Player 1: secret=2000" $TEST_DIR/NODE1.TXT ||
+       ! grep -q "Player 2: secret=1000" $TEST_DIR/NODE1.TXT; then
         echo "Wrong or missing secrets for '$1' '$2' #2: "
-        diff -u /dev/null $TEST_DIR/ANSWER.TXT
+        diff -u /dev/null $TEST_DIR/NODE1.TXT
         exit 1
     fi
 
     # Force player at both works as long as they're consistent.
-    start_answerer $1 -player2
-    start_dialer $2 -player1
+    start_node1 $1 -player2
+    start_node2 $2 -player1
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT ||
-       ! grep -q "Player 1: secret=2000" $TEST_DIR/ANSWER.TXT ||
-       ! grep -q "Player 2: secret=1000" $TEST_DIR/ANSWER.TXT; then
+    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
+       ! grep -q "Player 1: secret=2000" $TEST_DIR/NODE1.TXT ||
+       ! grep -q "Player 2: secret=1000" $TEST_DIR/NODE1.TXT; then
         echo "Wrong or missing secrets for '$1' '$2' #3: "
-        diff -u /dev/null $TEST_DIR/ANSWER.TXT
+        diff -u /dev/null $TEST_DIR/NODE1.TXT
         exit 1
     fi
 }
 
 run_player_tests_non_bg() {
     # Force player at dial.
-    start_answerer $1
-    start_dialer $2 -player1
+    start_node1 $1
+    start_node2 $2 -player1
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/ANSWER.TXT $TEST_DIR/DIAL.TXT ||
-       ! grep -q "Player 1: secret=2000" $TEST_DIR/ANSWER.TXT ||
-       ! grep -q "Player 2: secret=1000" $TEST_DIR/ANSWER.TXT; then
+    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
+       ! grep -q "Player 1: secret=2000" $TEST_DIR/NODE1.TXT ||
+       ! grep -q "Player 2: secret=1000" $TEST_DIR/NODE1.TXT; then
         echo "Wrong or missing secrets for '$1' '$2' #4: "
-        diff -u /dev/null $TEST_DIR/ANSWER.TXT
+        diff -u /dev/null $TEST_DIR/NODE1.TXT
         exit 1
     fi
 }

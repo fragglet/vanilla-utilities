1adc221fb5e27717d185532357c8d11f325cc505
Simon Howard <fraggle@soulsphere.org>
2023-07-17 18:57:37 -0400

    Run tests in CI too.

---
+++ test/runtests.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+tests="
+  ipxsetup_test
+  metanet_test
+  sersetup_test
+  sirsetup_test
+"
+
+result=0
+
+for t in $tests; do
+	if ./test/${t}.sh; then
+		echo "PASS: $t"
+	else
+		echo "FAIL: $t"
+		result=1
+	fi
+done
+
+exit $result
+

e84379652c6c616d5113acc8efe8beb8b36d7b70
Simon Howard <fraggle@soulsphere.org>
2023-08-24 16:40:02 -0400

    Add UDPSETUP-HOWTO to shipped documentation.

--- mkrelease.sh
+++ mkrelease.sh
@@ -8,7 +8,9 @@ cp -R bld/*.EXE modemcfg/ vutils.ico dist/
 cp COPYING.GPL1 dist/COPYING1.txt
 cp COPYING.md dist/COPYING.txt
 cp METANET-HOWTO.md dist/METANET.txt
-unix2dos dist/COPYING1.txt dist/COPYING.txt dist/METANET.txt
+cp UDPSETUP-HOWTO.md dist/UDPSETUP.txt
+unix2dos dist/COPYING1.txt dist/COPYING.txt dist/METANET.txt \
+         dist/UDPSETUP.txt
 
 cd dist
 zip -X -r ../vanilla-utilities-${1:-unknown-version}.zip *

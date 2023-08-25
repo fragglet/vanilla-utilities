aad70d6241fd7c0cce93614b54da0dc48bf7c846
Simon Howard <fraggle@soulsphere.org>
2023-08-25 15:10:33 -0400

    Add -l parameter to log wmake errors.

--- build/watbuild.sh
+++ build/watbuild.sh
@@ -3,7 +3,7 @@
 # Performs OpenWatcom build inside a background dosbox.
 # This should be run from the top-level directory.
 
-build/doscmd.sh wmake -e -f makefile.wat "$@"
+build/doscmd.sh wmake -e -f makefile.wat -l wmake.err "$@"
 result=$?
 
 touch NOTHING.ERR

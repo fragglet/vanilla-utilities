af3ef9ad6189a0ca04acba8980639fe18b4d0946
Simon Howard <fraggle@soulsphere.org>
2023-08-30 00:34:25 -0400

    Refactor duplicated test code, add better reporting.
    This matches the recent changes to sersetup_test.

--- test/sirsetup_test.sh
+++ test/sirsetup_test.sh
@@ -21,58 +21,49 @@ start_node2() {
 END
 }
 
+check_player_secrets() {
+    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT; then
+        echo "$1: fakedoom output from node1 and node2 does not match."
+        exit 1
+    fi
+    if ! grep -q "$2" $TEST_DIR/NODE1.TXT; then
+        echo "$1: fakedoom output does not contain '$2':"
+        diff -u /dev/null $TEST_DIR/NODE1.TXT
+        exit 1
+    fi
+    if ! grep -q "$3" $TEST_DIR/NODE1.TXT; then
+        echo "$1: fakedoom output does not contain '$3':"
+        diff -u /dev/null $TEST_DIR/NODE1.TXT
+        exit 1
+    fi
+}
+
 run_player_tests() {
+    test_descr="run_player_tests '$1' '$2'"
+
     # Simple dial and answer.
     start_node1 $1
     start_node2 $2
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
-       ! grep -q secret=1000 $TEST_DIR/NODE1.TXT ||
-       ! grep -q secret=2000 $TEST_DIR/NODE1.TXT; then
-        echo "Wrong or missing secrets for '$1' '$2' #1: "
-        diff -u /dev/null $TEST_DIR/NODE1.TXT
-        exit 1
-    fi
+    check_player_secrets "$test_descr, simple" \
+        "secret=1000" "secret=2000"
 
-    # Force player at answer.
+    # Force player
     start_node1 $1 -player2
     start_node2 $2
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
-       ! grep -q "Player 1: secret=2000" $TEST_DIR/NODE1.TXT ||
-       ! grep -q "Player 2: secret=1000" $TEST_DIR/NODE1.TXT; then
-        echo "Wrong or missing secrets for '$1' '$2' #2: "
-        diff -u /dev/null $TEST_DIR/NODE1.TXT
-        exit 1
-    fi
+    check_player_secrets "$test_descr, force player" \
+        "Player 1: secret=2000" "Player 2: secret=1000"
 
     # Force player at both works as long as they're consistent.
     start_node1 $1 -player2
     start_node2 $2 -player1
     wait_dosboxes
 
-    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
-       ! grep -q "Player 1: secret=2000" $TEST_DIR/NODE1.TXT ||
-       ! grep -q "Player 2: secret=1000" $TEST_DIR/NODE1.TXT; then
-        echo "Wrong or missing secrets for '$1' '$2' #3: "
-        diff -u /dev/null $TEST_DIR/NODE1.TXT
-        exit 1
-    fi
-
-    # Force player at dial.
-    start_node1 $1
-    start_node2 $2 -player1
-    wait_dosboxes
-
-    if ! diff -u $TEST_DIR/NODE1.TXT $TEST_DIR/NODE2.TXT ||
-       ! grep -q "Player 1: secret=2000" $TEST_DIR/NODE1.TXT ||
-       ! grep -q "Player 2: secret=1000" $TEST_DIR/NODE1.TXT; then
-        echo "Wrong or missing secrets for '$1' '$2' #4: "
-        diff -u /dev/null $TEST_DIR/NODE1.TXT
-        exit 1
-    fi
+    check_player_secrets "$test_descr, force player at both" \
+        "Player 1: secret=2000" "Player 2: secret=1000"
 }
 
 # For testing null modem connections we just use COM2.

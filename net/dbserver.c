fragglet/vanilla-utilities

commit 73caaab566af6c9109b1b9350110f42356c364f7

udpsetup: Add statistics to DOSbox server code
This resolves some TODOs as we are now recording when errors occur.

net/dbserver.c
@@ -19,6 +19,7 @@
 #include "net/doomnet.h"
 #include "net/dossock.h"
 #include "net/ipxnet.h"
+#include "net/pktstats.h"
 
 extern const ipx_addr_t broadcast_addr;
 const ipx_addr_t null_addr = {0, {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}};
@@ -28,6 +29,36 @@ static int server_num_clients = 0;
 
 static packet_t packet;
 static SOCKET server_sock = INVALID_SOCKET;
+
+DECLARE_COUNTER(server_rx_packets);
+DECLARE_COUNTER(server_rx_errors);
+DECLARE_COUNTER(server_tx_packets);
+DECLARE_COUNTER(server_tx_errors);
+
+static void RegisterCounters(void)
+{
+    REGISTER_COUNTER(server_rx_packets);
+    REGISTER_COUNTER(server_rx_errors);
+    REGISTER_COUNTER(server_tx_packets);
+    REGISTER_COUNTER(server_tx_errors);
+}
+
+static ssize_t SendtoOrLog(SOCKET socket, const void far *msg, size_t len,
+                           int flags, const struct sockaddr_in far *to)
+{
+    ssize_t result = sendto(socket, msg, len, flags, to);
+
+    if (result < 0)
+    {
+        INCREMENT_COUNTER(server_tx_errors);
+    }
+    else
+    {
+        INCREMENT_COUNTER(server_tx_packets);
+    }
+
+    return result;
+}
 
 static void SockaddrToIPX(struct sockaddr_in *inaddr, ipx_addr_t *ipxaddr)
 {
@@ -66,21 +97,16 @@ static void ForwardPacket(struct sockaddr_in *src_addr, int len)
     if (memcmp(&packet.ipx.Dest, &broadcast_addr, sizeof(ipx_addr_t)) != 0)
     {
         IPXToSockaddr(&packet.ipx.Dest, &dest_addr);
-        if (sendto(server_sock, &packet, len, 0, &dest_addr) < 0)
-        {
-            // TODO: log error
-        }
+        SendtoOrLog(server_sock, &packet, len, 0, &dest_addr);
         return;
     }
 
     for (i = 0; i < server_num_clients; i++)
     {
         if (memcmp(&server_client_addrs[i], src_addr,
-                   sizeof(struct sockaddr_in)) != 0
-         && sendto(server_sock, &packet, len, 0,
-                   &server_client_addrs[i]) < 0)
+                   sizeof(struct sockaddr_in)) != 0)
         {
-            // TODO: log error
+            SendtoOrLog(server_sock, &packet, len, 0, &server_client_addrs[i]);
         }
     }
 }
@@ -118,10 +144,7 @@ static void NewClient(struct sockaddr_in *addr)
     reply.Src.Network = htonl(1);
     reply.SrcSocket = htons(2);
 
-    if (sendto(server_sock, &reply, sizeof(ipx_header_t), 0, addr) < 0)
-    {
-        // TODO: log error
-    }
+    SendtoOrLog(server_sock, &reply, sizeof(ipx_header_t), 0, addr);
 }
 
 static void SendShutdown(void)
@@ -145,8 +168,8 @@ static void SendShutdown(void)
 
         for (i = 0; i < 3; i++)
         {
-            if (sendto(server_sock, &msg, sizeof(ipx_header_t), 0,
-                       &server_client_addrs[c]) < 0)
+            if (SendtoOrLog(server_sock, &msg, sizeof(ipx_header_t), 0,
+                            &server_client_addrs[c]) < 0)
             {
                 LogMessage("Error sending shutdown packet: errno=%d",
                            DosSockLastError);
@@ -171,9 +194,10 @@ void RunServer(void)
         len = recvfrom(server_sock, &packet, sizeof(packet), 0, &addr);
         if (len < 0)
         {
-            // TODO: WSAEWOULDBLOCK is expected, other errors are not.
-            break;
+            INCREMENT_COUNTER(server_rx_errors);
+            continue;
         }
+        INCREMENT_COUNTER(server_rx_packets);
         if (IsRegistrationPacket(&packet.ipx))
         {
             NewClient(&addr);
@@ -200,6 +224,8 @@ void StartServer(uint16_t port)
 {
     unsigned long trueval = 1;
     struct sockaddr_in bind_addr = {AF_INET, 0, {INADDR_ANY}};
+
+    RegisterCounters();
 
     server_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (server_sock == INVALID_SOCKET)

makefile.tc
@@ -42,10 +42,11 @@ tests: test\fakedoom.exe
 bld\common.lib: lib\flag.obj lib\log.obj lib\dos.obj lib\ints.obj
 	del $@
 	tlib $@ +lib\flag.obj +lib\log.obj +lib\dos.obj +lib\ints.obj
-bld\udpipx.lib: net\udpipx.obj net\llcall.obj net\dossock.obj net\dbserver.obj
+bld\udpipx.lib: net\udpipx.obj net\llcall.obj net\dossock.obj \
+                net\dbserver.obj net\pktstats.obj
 	del $@
 	tlib $@ +net\udpipx.obj +net\llcall.obj +net\dossock.obj
-	tlib $@ +net\dbserver.obj
+	tlib $@ +net\dbserver.obj +net\pktstats.obj
 bld\replay.exe: $(REPLAY_OBJS)
 	tcc -e$* -oreplay $(REPLAY_OBJS)
 bld\analogjs.exe: $(ANALOGJS_OBJS)

makefile.wat
@@ -16,8 +16,7 @@ SIRSETUP_OBJS = bld\sirsetup.o bld\doomnet.o bld\serport.o bld\pktaggr.o &
                 bld\common.lib
 PARSETUP_OBJS = bld\parsetup.o bld\doomnet.o bld\parport.o bld\plio.o &
                 bld\serarb.o bld\common.lib
-UDPSETUP_OBJS = bld\ipxsetup.o bld\doomnet.o bld\udpipx.o bld\llcall.o &
-                bld\common.lib bld\dossock.o bld\dbserver.o
+UDPSETUP_OBJS = bld\ipxsetup.o bld\doomnet.o bld\udpipx.lib bld\common.lib
 PASSTHRU_OBJS = bld\passthru.o bld\doomnet.o bld\common.lib
 SOLO_NET_OBJS = bld\solo-net.o bld\doomnet.o bld\common.lib
 METANET_OBJS = bld\metanet.o bld\doomnet.o bld\common.lib bld\pktaggr.o &
@@ -43,6 +42,9 @@ all: $(EXES) $(TESTS)
 
 bld\common.lib: bld\flag.o bld\log.o bld\dos.o bld\ints.o
 	wlib -q -n $@ +bld\flag.o +bld\log.o +bld\dos.o +bld\ints.o
+bld\udpipx.lib: bld\udpipx.o bld\llcall.o bld\dossock.o bld\dbserver.o bld\pktstats.o
+	wlib -q -n $@ +bld\udpipx.o +bld\llcall.o +bld\dossock.o &
+                      +bld\dbserver.o +bld\pktstats.o
 bld\replay.exe: $(REPLAY_OBJS)
 	wcl -q -fe=$@ $(REPLAY_OBJS)
 bld\analogjs.exe: $(ANALOGJS_OBJS)

net/udpipx.c
@@ -26,6 +26,7 @@
 #include "net/dbserver.h"
 #include "net/dossock.h"
 #include "net/ipxnet.h"
+#include "net/pktstats.h"
 
 #define DEFAULT_UDP_PORT  213  /* as used by dosbox */
 
@@ -66,6 +67,7 @@ void IPXRegisterFlags(void)
     UnsignedIntFlag("-ipxsocket", &ipxsocket, "socket", NULL);
     UnsignedIntFlag("-udpport", &udpport, "port",
                     "UDP port that server should use, default 213");
+    PacketStatsRegisterFlags();
 }
 
 static void ParseServerAddress(const char *addr)

commit de4b90df26ea18653e7d3f0d4c4f46db50535ec9

udpsetup: Fix bug with server code
Accidentally introduced in commit 73caaab566af6c9109b1b93.

net/dbserver.c
@@ -194,8 +194,13 @@ void RunServer(void)
         len = recvfrom(server_sock, &packet, sizeof(packet), 0, &addr);
         if (len < 0)
         {
-            INCREMENT_COUNTER(server_rx_errors);
-            continue;
+            // WSAEWOULDBLOCK is expected when there are no more packets to
+            // process; other errors are not.
+            if (DosSockLastError != WSAEWOULDBLOCK)
+            {
+                INCREMENT_COUNTER(server_rx_errors);
+            }
+            break;
         }
         INCREMENT_COUNTER(server_rx_packets);
         if (IsRegistrationPacket(&packet.ipx))
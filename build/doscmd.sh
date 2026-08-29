2ef681dd2f8a3b269a4dbad2a8ff46174baf13bf
Simon Howard <fraggle@soulsphere.org>
2026-08-29 01:39:24 -0400

    [build] Eliminate references to build.log
    This is no longer used now we have `dbpipe`.

--- build/doscmd.sh
+++ build/doscmd.sh
@@ -8,8 +8,6 @@
 # How this works:
 # * a temporary batch file called "thecmd.bat" is generated that gets
 #   run inside dosbox by a wrapper batch file named doscmd.bat.
-# * the DOS stdout of the command (not stderr) gets written to build.log
-#   and then gets passed through to this script's stdout
 # * the exit code is written to a temporary file named result.txt and
 #   passed back to this script (although only success/failure; not the
 #   full exit code)
